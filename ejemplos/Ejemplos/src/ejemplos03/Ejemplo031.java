ca2b584e7cc48e1a1a88b72bf2949b0bbae95504
unknown <luiseduardosa2006@gmail.com>
2025-01-08 12:16:29 -0500

    Enner

--- ejemplos/Ejemplos/src/ejemplos03/Ejemplo031.java
+++ ejemplos/Ejemplos/src/ejemplos03/Ejemplo031.java
@@ -1,40 +1,46 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+generar una solucion que permita sumar 2 numeros a travez de un metodo, 
+los numeros deben ser ingresados por teclado en el main
+el metodo que genera la suma debe verificar que los dos numeros sean positivos para realizar la operacion
+caso contrario debe devolver suma como 0
  */
-package ejemplos01;
+package ejemplos03;
+
 import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
-public class Ejemplo03 {
+public class Ejemplo031 {
 
     public static void main(String[] args) {
         // 
-        Scanner entrada= new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);
         int valor1;
         int valor2;
-        
+
         System.out.println("ingrese el primer valor");
         valor1 = entrada.nextInt();
         System.out.println("ingrese el segundo valor");
         valor2 = entrada.nextInt();
         int valor = obtenerSuma(valor1, valor2); // se invoca al método 
-                                         // obtenerSuma
+        // obtenerSuma
         System.out.printf("El valor de la suma es: %d\n", valor);
-        
+
         // System.out.printf("El valor de la suma es: %d\n", obtenerSuma(10, 30));
     }
-    
-    
-    public static int obtenerSuma(int a, int b){
+
+    public static int obtenerSuma(int a, int b) {
         int suma;
-        suma = a + b;
+        if (a > 0 && b > 0) {
+            suma = a + b;
+        } else {
+            suma = 0;
+        }
+        
         return suma;
         // return a + b;
     }
-    
-    
+
 }
